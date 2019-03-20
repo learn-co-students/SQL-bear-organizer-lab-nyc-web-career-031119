11759bb6da43a8360ed6b34af81a23c91d5fd0d1
david bernier <davidbernier91@gmail.com>
2019-03-19 22:15:07 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Mr. Chocolate', 4, 'M', 'brown', 'shy', 1);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Rowdy', 5, 'M', 'black', 'aggressive', 0);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('tabitha', 1, 'F', 'white', 'aggressive', 1);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Sergant Brown', 3, 'M', 'panda', 'shy', 0);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Melissa', 2, 'F', 'brown', 'calm', 1);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Grinch', 1, 'M', 'white', 'friendly', 1);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ ('Werndy', 10, 'F', 'yellow', 'aggresssive', 0);
+ 
+INSERT INTO 
+bears (name, age, gender, color, temperament, alive) 
+VALUES
+ (null, 10, 'F', 'yellow', 'aggresssive', 0);
+ 

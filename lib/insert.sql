6927c0fc088a8ecf89dfac717098054ee77acb89
Locallize <Fillanlee@aol.com>
2019-03-20 13:10:47 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,18 +7,18 @@
 -- Wendy
 -- sqlite3 pets_database.db < 01_insert_cats_into_cats_table.sql
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate",4, "male", "green", "good", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate",4, "male", "green", "good", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "female", "brown", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "female", "brown", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "female", "brown", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "female", "brown", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "male", "blue", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "male", "blue", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "female", "brown", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "female", "brown", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "female", "brown", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "female", "brown", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "male", "brown", "calm",false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "male", "brown", "calm",0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 1, "female", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 1, "female", "brown", "calm", 1);

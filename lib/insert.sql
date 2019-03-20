2e8f58660773c4cced4e152dcb9a5a788b274790
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-03-20 17:18:00 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergent', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'male', 'blue', 'supernice', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, 'male', 'blue', 'supernice', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'blue', 'supernice', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'male', 'blue', 'supernice', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'blue', 'supernice', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergent', 2, 'male', 'blue', 'supernice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'male', 'blue', 'supernice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'blue', 'supernice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'male', 'blue', 'supernice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 2, 'male', 'blue', 'supernice', 1);
